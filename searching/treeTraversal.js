a872b02af8a66452567ac91a018f363bfbe830ba
Abraham Cuenca <websiteprojectmanager@gmail.com>
2018-03-07 12:09:30 -0800

    Add preOrder and postOrder functions as well as bst diagram in comments

--- searching/treeTraversal.js
+++ searching/treeTraversal.js
@@ -22,7 +22,43 @@ function inOrder(tree, results=[]){
     return results;
   }
 
-  // preOrder(){}
-  // postOrder(){}
+  function preOrder(tree, results=[]){
+    results.push(tree.key);
+    if(tree.left){
+      preOrder(tree.left, results);
+    }
+    if(tree.right){
+      preOrder(tree.right, results)
+    }
 
+    return results;
+  }
+
+  function postOrder(tree, results=[]){
+    if(tree.left){
+      postOrder(tree.left, results);
+    }
+    if(tree.right){
+      postOrder(tree.right, results);
+    }
+    results.push(tree.key);
+
+    return results;
+  }
+
+console.log(dataset);
 console.log(inOrder(bst));
+console.log(preOrder(bst));
+console.log(postOrder(bst));
+
+/*
+                           25
+                      /          \
+                     15           50
+                   /   \         /   \
+                 10     24     35     70
+                /  \   /  \    / \    / \
+               4   12 18     31   44 66  90
+                        \
+                        22
+*/
